547561925bf19ee284871ee3e079e52e5fa5071e
Zeleo21 <leo.guazzaroni@epita.fr>
2024-09-25 21:34:06 +0200

    feat: added the GetAllUsers endpoint

--- backend/models/User.go
+++ backend/models/User.go
@@ -38,6 +38,14 @@ func (user *User) BeforeCreate(tx *gorm.DB) (err error) {
 	return
 }
 
+func (user *User) GetAllUsers(db *gorm.DB) ([]User, error) {
+	var users []User
+	if err := db.Find(&users).Error; err != nil {
+		return nil, err
+	}
+	return users, nil
+}
+
 func (user *User) FindById(db *gorm.DB, userId uuid.UUID) (User, error) {
 	var foundUser User
 	if err := db.First(&foundUser, "id = ?", userId).Error; err != nil {
