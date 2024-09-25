4810b487e16fc68f10cbc0300f224f08136f8e53
Zeleo21 <leo.guazzaroni@epita.fr>
2024-09-25 21:28:21 +0200

    feat: added CRUD operations to User

---
+++ backend/routes/user.go
@@ -0,0 +1,22 @@
+package routes
+
+import (
+	"tms-backend/controllers"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+func UserRoutes(r *gin.Engine, db *gorm.DB) *gin.Engine {
+	UserController := controllers.UserController{
+		Db: db,
+	}
+	v1 := r.Group("/api/v1/users")
+	{
+		v1.GET("/:id", UserController.GetUser)
+		v1.POST("/", UserController.CreateUser)
+		v1.PATCH("/:id", UserController.UpdateUser)
+		v1.DELETE("/:id", UserController.DeleteUser)
+	}
+	return r
+}
