89e1229135f88016c1d7a92595b0fe8dfd7572d1
Zeleo21 <leo.guazzaroni@epita.fr>
2024-09-25 21:28:21 +0200

    feat: added CRUD operations to User

---
+++ backend/controllers/User.go
@@ -0,0 +1,90 @@
+package controllers
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+	"net/http"
+	"tms-backend/models"
+)
+
+type UserController struct {
+	Db *gorm.DB
+}
+
+func (UserController *UserController) GetUser(c *gin.Context) {
+	var user models.User
+	id := c.Param("id")
+	userId, err := uuid.Parse(id)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid userId"})
+		return
+	}
+
+	user, err = user.FindById(UserController.Db, userId)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, user)
+}
+
+func (UserController *UserController) CreateUser(c *gin.Context) {
+	var user models.User
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	if err := UserController.Db.Create(&user).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, user)
+}
+
+func (UserController *UserController) UpdateUser(c *gin.Context) {
+	var user models.User
+	id := c.Param("id")
+	userId, err := uuid.Parse(id)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid userId"})
+		return
+	}
+	user, err = user.FindById(UserController.Db, userId)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	var updateUser models.User
+	if err := c.ShouldBindJSON(&updateUser); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	if err := UserController.Db.Model(&user).Updates(updateUser).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, user)
+}
+
+func (UserController *UserController) DeleteUser(c *gin.Context) {
+	var user models.User
+	id := c.Param("id")
+	userId, err := uuid.Parse(id)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid userId"})
+		return
+	}
+	user, err = user.FindById(UserController.Db, userId)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	if err := UserController.Db.Delete(&user).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, user)
+}
--- backend/models/User.go
+++ backend/models/User.go
@@ -37,3 +37,16 @@ func (user *User) BeforeCreate(tx *gorm.DB) (err error) {
 	user.Id = uuid.New()
 	return
 }
+
+func (user *User) FindById(db *gorm.DB, userId uuid.UUID) (User, error) {
+	var foundUser User
+	if err := db.First(&foundUser, "id = ?", userId).Error; err != nil {
+		return User{}, err
+	}
+	return foundUser, nil
+}
+
+func (user *User) getRole(db *gorm.DB, userId uuid.UUID) (Role, error) {
+	db.First(&user, "id = ?", userId)
+	return user.Role, nil
+}
