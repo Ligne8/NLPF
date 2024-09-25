75e4610b693fca11394d3b246a526c81d683acc3
Zeleo21 <leo.guazzaroni@epita.fr>
2024-09-25 21:54:41 +0200

    feat: added the swagger documentation for users

--- backend/controllers/User.go
+++ backend/controllers/User.go
@@ -12,6 +12,16 @@ type UserController struct {
 	Db *gorm.DB
 }
 
+// GetUsers Retrieve users
+//
+//		@Summary      List users
+//		@Description  get all users
+//		@Tags         users
+//		@Accept       json
+//		@Produce      json
+//		@Success      200  {array}   models.User
+//	 	@Failure   	  500 "Unable to retrieve users"
+//		@Router       /users [get]
 func (UserController *UserController) GetUsers(c *gin.Context) {
 	var user models.User
 	users, err := user.GetAllUsers(UserController.Db)
@@ -22,6 +32,17 @@ func (UserController *UserController) GetUsers(c *gin.Context) {
 	c.JSON(http.StatusOK, users)
 }
 
+// GetUser Retrieve user
+//
+//		@Summary      user
+//		@Description  get a user by id
+//		@Tags         users
+//		@Accept       json
+//		@Produce      json
+//		@Success      200 {object} models.User
+//	 	@Failure   	  500 "Unable to retrieve user"
+//		@Failure   	  400 "Invalid userId"
+//		@Router       /user/:id [get]
 func (UserController *UserController) GetUser(c *gin.Context) {
 	var user models.User
 	id := c.Param("id")
@@ -40,6 +61,18 @@ func (UserController *UserController) GetUser(c *gin.Context) {
 	c.JSON(http.StatusOK, user)
 }
 
+// CreateUser Retrieve user
+//
+//		@Summary      user
+//		@Description  create a user
+//		@Tags         users
+//		@Accept       json
+//		@Produce      json
+//		@Param        user  body      models.User  true  "User"
+//		@Success      200 {object} models.User
+//	 	@Failure   	  500 "Unable to retrieve user"
+//		@Failure   	  400 "Invalid request"
+//		@Router       /user [post]
 func (UserController *UserController) CreateUser(c *gin.Context) {
 	var user models.User
 	if err := c.ShouldBindJSON(&user); err != nil {
@@ -53,6 +86,18 @@ func (UserController *UserController) CreateUser(c *gin.Context) {
 	c.JSON(http.StatusOK, user)
 }
 
+// UpdateUser Retrieve user
+//
+//		@Summary      user
+//		@Description  updates a user
+//		@Tags         users
+//		@Accept       json
+//		@Produce      json
+//		@Param        user  body models.User  true  "User"
+//		@Success      200 {object} models.User
+//	 	@Failure   	  500 "Unable to retrieve user"
+//		@Failure   	  400 "Invalid request"
+//		@Router       /user/:id [patch]
 func (UserController *UserController) UpdateUser(c *gin.Context) {
 	var user models.User
 	id := c.Param("id")
@@ -79,6 +124,17 @@ func (UserController *UserController) UpdateUser(c *gin.Context) {
 	c.JSON(http.StatusOK, user)
 }
 
+// DeleteUser Retrieve user
+//
+//		@Summary      user
+//		@Description  deletes a user
+//		@Tags         users
+//		@Accept       json
+//		@Produce      json
+//		@Success      200 {object} models.User
+//	 	@Failure   	  500 "Unable to retrieve user or unable to delete user"
+//		@Failure   	  400 "Invalid request"
+//		@Router       /user/:id [delete]
 func (UserController *UserController) DeleteUser(c *gin.Context) {
 	var user models.User
 	id := c.Param("id")
