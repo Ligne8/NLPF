bb8a0ed219cf68fad906760c1119040e2ab16fa8
tom.ami <tom.ami@epita.fr>
2024-09-30 16:38:25 +0200

    LIG-58 LIG-52 : feat les deux tickets

--- backend/main.go
+++ backend/main.go
@@ -1,15 +1,16 @@
 package main
 
 import (
-	"github.com/gin-contrib/cors"
-	"github.com/gin-gonic/gin"
-	swaggerfiles "github.com/swaggo/files"
-	ginSwagger "github.com/swaggo/gin-swagger"
 	"time"
 	"tms-backend/database"
 	docs "tms-backend/docs"
 	"tms-backend/models"
 	"tms-backend/routes"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 // swagger embed files
@@ -18,12 +19,12 @@ func main() {
 	router := gin.Default()
 
 	router.Use(cors.New(cors.Config{
-		AllowAllOrigins:  true,
-		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD"},
-		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"},
 		ExposeHeaders:    []string{"Content-Length"},
 		AllowCredentials: true,
 		MaxAge:           12 * time.Hour,
+		AllowHeaders:     []string{"Strict-Transport-Security", "strict-origin-when-cross-origin", "Content-Type"},
 	}))
 
 	db := database.InitDb()
