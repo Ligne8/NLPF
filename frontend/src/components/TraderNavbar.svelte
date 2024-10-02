2ea2f3749f59cf48f4a0c588e54e8eb354448883
Mathieu MARIN <mathieu.marin@epita.fr>
2024-10-02 21:28:37 +0200

    feat: add sub navbar for trader

---
+++ frontend/src/components/TraderNavbar.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+    import { onMount } from 'svelte';
+    import { currentTrafficManagerTab } from '../stores/store.js';
+
+    // Function to switch tab
+    function switchTab(tab: string) {
+        currentTrafficManagerTab.set(tab);
+    }
+
+    // Set tab based on URL on mount
+    onMount(() => {
+        const path = window.location.pathname;
+        if (path.includes('trader/lots'))
+            currentTrafficManagerTab.set('Lots');
+        else if (path.includes('trader/tractors'))
+            currentTrafficManagerTab.set('Tractors');
+        else
+            currentTrafficManagerTab.set(''); // Reset if on an unknown path
+    });
+</script>
+
+
+<!-- Sub-Navbar -->
+<nav class="bg-gray-200 px-10 py-4 text-gray-800">
+    <ul class="flex space-x-16">
+        <li>
+            <a
+                    href="/trader/lots"
+                    on:click={() => switchTab('Lots')}
+                    class="{$currentTrafficManagerTab === 'Lots' ? 'font-bold bg-gray-800 text-gray-200 rounded-md px-4 py-2' : ''}">Lots
+            </a>
+        </li>
+        <li>
+            <a
+                    href="/trader/tractors"
+                    on:click={() => switchTab('Tractors')}
+                    class="{$currentTrafficManagerTab === 'Tractors' ? 'font-bold bg-gray-800 text-gray-200 rounded-md px-4 py-2' : ''}">Tracteurs
+            </a>
+        </li>
+    </ul>
+</nav>
--- frontend/src/pages/trader/tractors/tractors.svelte
+++ frontend/src/pages/trader/tractors/tractors.svelte
@@ -1,6 +1,9 @@
 <script>
     import Navbar from "@components/Navbar.svelte";
+    import TraderNavbar from "@components/TraderNavbar.svelte";
 </script>
     
 <Navbar/>
+<TraderNavbar/>
+
 <h1>trader/tractors</h1>
