3e2ed7213aad5c6c1874956c1f5154d0c8c4598e
Mathieu MARIN <mathieu.marin@epita.fr>
2024-10-08 10:39:07 +0200

    fix: merge problem

--- frontend/src/pages/stock_exchange/lots/lots.svelte
+++ frontend/src/pages/stock_exchange/lots/lots.svelte
@@ -80,7 +80,7 @@
                 <th class="border p-2 text-center">Current price<br><span class="font-normal">(in €/km)</span></th>
                 {#if $userRole === "client"}
                     <th class="border p-2 text-center">Actions</th>
-                {/if}    
+                {/if}
             </tr>
         </thead>
         <tbody>
